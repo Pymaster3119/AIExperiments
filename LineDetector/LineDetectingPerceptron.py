3e7951347d0cc16c43ca90f403bb2d7e4bf37d8d
pymaster9 <adi31tya@icloud.com>
2024-07-15 22:43:01 -0400

    Finished code for perceptron, don't know if it works

---
+++ LineDetector/LineDetectingPerceptron.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+y_int = 0
+slope = 0
+points = []
+batch = 0
+
+class point:
+    def __init__ (self, x, y, classification, truth):
+        self.x = x
+        self.y = y
+        self.classification = classification
+        self.truth = truth
+
+
+def plot():
+    global points
+    truenegatives = 0
+    falsenegatives = 0
+    truepositives = 0
+    falsepositives = 0
+    for x in points:
+        if (x.classification == True and x.truth == True):
+            truepositives += 1
+        if (x.classification == False and x.truth == True):
+            falsenegatives += 1
+        if (x.classification == True and x.truth == False):
+            falsepositives += 1
+        if (x.classification == False and x.truth == False):
+            truenegatives += 1
+    with (open("LineDetector/ModelStats/" + str(batch) +'.txt', "w") as txt):
+        txt.write("True Negatives: " + str(truenegatives))
+        txt.write("False Negatives: " + str(falsenegatives))
+        txt.write("True Positives: " + str(truepositives))
+        txt.write("False Positives: " + str(falsepositives))
+
+
+def save_model():
+    global weights, bias
+    with (open("LineDetector/ModelSaves/" + str(batch) +'.txt', "w") as txt):
+        txt.write(str(bias) + "\n")
+        for x in range(len(weights)):
+            txt.write(str(weights[x]) + "\n")
+#Set up network
+neuronCount = 4
+weights = []
+for i in range(neuronCount):
+    weights.append(random.random() * 2 - 1)
+bias = random.random() * 2 - 1
+learning_rate = 1
+
+
+for batch in range(100):
+    for i in range(1000):
+        y_int = random.random() * 20 - 10
+        slope = random.random() * 20 - 10
+        inputx=random.random() * 100
+        inputy=random.random() * 100 * slope
+        neuronvalues = [inputx, inputy, y_int, slope]
+        value = 0
+        for x in range(neuronCount):
+            value += weights[x] * neuronvalues[x] + bias
+        value = min(max(0,round(value)), 1)
+        above = False
+        if(value > 0.5):
+            above = True
+        
+        truth = 1 if inputy > (slope * inputx) + y_int else 0
+        points.append(point(inputx, inputy, above, truth==1))
+        error = truth - value
+        for x in range(neuronCount):
+            weights[x] = weights[x] + learning_rate * error * neuronvalues[x]
+        bias = bias + learning_rate * error
+    plot()
+    save_model()
+    points = []
+    learning_rate = pow(0.9,batch)
+    print("Batch " + str(batch) + " Learning Rate " + str(learning_rate))
