8196f9c9513b25eaf4d7490d68d874f37b11e280
pymaster9 <adi31tya@icloud.com>
2024-07-17 15:53:40 -0400

    Making it draw lines

--- MatrixTransform/matrix.py
+++ MatrixTransform/matrix.py
@@ -13,30 +13,30 @@ def generate_square_points():
     global points
 
     # Generate points for the bottom side of the square
-    for x in range(0,10):
-        points.append((x, 0))
+    for x in range(20,31):
+        points.append((x, 20))
 
     # Generate points for the right side of the square
-    for y in range(0,10):
-        points.append((10, y))
+    for y in range(20,31):
+        points.append((30, y))
 
     # Generate points for the top side of the square
-    for x in range(10, -1, -1):
-        points.append((x, 10))
+    for x in range(30, 19, -1):
+        points.append((x, 30))
 
     # Generate points for the left side of the square
-    for y in range(10,-1, -1):
-        points.append((0, y))
+    for y in range(30,19, -1):
+        points.append((20, y))
 
 def create_visualizer():
-    global canvas, points
+    global canvas, points, matrixa, matrixb, matrixc, matrixd, graphFrom, graphTo
     print(len(points))
     fig = Figure(figsize = (5, 5), dpi = 100)
     axes1 = fig.add_axes([0.1,0.1,0.9,0.9])
-    axes1.set_ylim(0, 10.5)
-    axes1.set_xlim(0, 10.5)
+    axes1.set_ylim(int(graphFrom.get()), int(graphTo.get()))
+    axes1.set_xlim(int(graphFrom.get()), int(graphTo.get()))
     #Draw axes
-    for x in range(-40, 40):
+    for x in range(-100 + round(axes1.get_ylim()[0]), 100 +round(axes1.get_ylim()[1])):
         start = [x,-100]
         start = [start[0] * float(matrixa.get()) + start[1] * float(matrixb.get()), start[0] * float(matrixc.get()) + start[1] * float(matrixd.get())]
         end = [x, 100]
@@ -44,7 +44,7 @@ def create_visualizer():
         xpoints = numpy.array([start[0],end[0]])
         ypoints = numpy.array([start[1],end[1]])
         axes1.plot(xpoints, ypoints, color = "gray")
-    for y in range(-40, 40):
+    for y in range(-100, 100):
         start = [-100,y]
         start = [start[0] * float(matrixa.get()) + start[1] * float(matrixb.get()), start[0] * float(matrixc.get()) + start[1] * float(matrixd.get())]
         end = [100,y]
@@ -81,5 +81,11 @@ pointy = tkinter.StringVar(frame)
 tkinter.Entry(frame, textvariable=pointx).grid(row=4, column=0)
 tkinter.Entry(frame, textvariable=pointy).grid(row=4, column=1)
 tkinter.Button(frame, text="create point", command=create_point).grid(row=5, column=0)
+graphFrom = tkinter.StringVar(frame, "0")
+graphTo = tkinter.StringVar(frame, "50")
+tkinter.Label(frame, text = "Graph from").grid(row=6, column=0)
+tkinter.Label(frame, text = "Graph to").grid(row=6, column=1)
+tkinter.Entry(frame, textvariable=graphFrom).grid(row=7, column=0)
+tkinter.Entry(frame, textvariable=graphTo).grid(row=7, column=1)
 create_visualizer()
 root.mainloop()
