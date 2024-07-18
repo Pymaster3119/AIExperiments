298e7f862fcf10333f51e5d9c2f0ad9753615255
pymaster9 <adi31tya@icloud.com>
2024-07-18 00:50:26 -0400

    Fixing bugs in image import

--- MatrixTransform/matrix.py
+++ MatrixTransform/matrix.py
@@ -12,7 +12,8 @@ canvas = tkinter.Canvas(frame,width=300, height=300)
 canvas.grid(row=0, column=0)
 points = []
 def generate_square_points():
-    global points
+    global points, status
+    status.set("Status: creating square...")
 
     # Generate points for the bottom side of the square
     for x in range(20,31):
@@ -29,9 +30,11 @@ def generate_square_points():
     # Generate points for the left side of the square
     for y in range(30,19, -1):
         points.append((20, y))
+    status.set("Status: idle")
 
 def create_visualizer():
-    global canvas, points, matrixa, matrixb, matrixc, matrixd, graphFrom, graphTo
+    global canvas, points, matrixa, matrixb, matrixc, matrixd, graphFrom, graphTo, status
+    status.set("Status: drawing plots...")
     print(len(points))
     fig = Figure(figsize = (5, 5), dpi = 100)
     axes1 = fig.add_axes([0.1,0.1,0.9,0.9])
@@ -67,34 +70,37 @@ def create_visualizer():
     canvas = FigureCanvasTkAgg(fig,  master = frame)
     canvas.draw()
     canvas.get_tk_widget().grid(row=0, column=0)
+    status.set("Status: idle")
 def create_point():
-    global points, pointx, pointy
+    global points, pointx, pointy, status
     points.append((float(pointx.get()),float(pointy.get())))
 def import_image():
-    global points, imageFrom, imageTo
-    filename = askopenfilename()
+    global points, imageFromX, imageToX, imageFromY, imageToY, status
+    status.set("Status: importing image...")
+    filename = askopenfilename(filetypes=[("JPG", "*.jpg")])
     image = imread(filename)
-    delta = float(imageTo.get()) - float(imageFrom.get())
+    deltaX = float(imageToX.get()) - float(imageFromX.get())
+    deltaY = float(imageToY.get()) - float(imageFromY.get())
 
     prevx = float('inf')
     prevy = float('-inf')
     xcount = 0
     ycount = 0
     for i in range(image.shape[0]):
-        x = float(imageTo.get())-(i / image.shape[0]) * delta 
+        x = float(imageToX.get())-(i / image.shape[0]) * deltaX 
         if (abs(prevx-x) > 0.1):
             prevx = x
             xcount+= 1
             prevy = float('-inf')
             ycount = 0
             for j in range(image.shape[1]):
-                y = (j / image.shape[1]) * delta + float(imageFrom.get())
+                y = (j / image.shape[1]) * deltaY + float(imageFromY.get())
                 if (y - prevy > 0.1):
                     prevy = y
                     ycount += 1
                     r, g, b = image[i][j][:3] / 255.0
-                    points.append((y, x, (r, g, b), "            "))
-    print(points)
+                    points.append((y, x, (r, g, b)))
+    status.set("Status: idle")
             
     
 def delete_points():
@@ -103,32 +109,38 @@ matrixa = tkinter.StringVar(frame, "1")
 matrixb = tkinter.StringVar(frame, "0")
 matrixc = tkinter.StringVar(frame, "0")
 matrixd = tkinter.StringVar(frame, "1")
+status = tkinter.StringVar(root, "Status: idle")
 tkinter.Entry(frame, textvariable=matrixa).grid(row=1,column=0)
 tkinter.Entry(frame, textvariable=matrixb).grid(row=1,column=1)
 tkinter.Entry(frame, textvariable=matrixc).grid(row=2,column=0)
 tkinter.Entry(frame, textvariable=matrixd).grid(row=2,column=1)
-tkinter.Button(frame, text="Create plot", command=create_visualizer).grid(row=3, column=0)
-tkinter.Button(frame, text="Create square", command=generate_square_points).grid(row = 3, column=1)
+tkinter.Button(frame, text="Create plot", command=lambda:root.after(1, create_visualizer)).grid(row=3, column=0)
+tkinter.Button(frame, text="Create square", command=lambda:root.after(1, generate_square_points)).grid(row = 3, column=1)
 pointx = tkinter.StringVar(frame)
 pointy = tkinter.StringVar(frame)
 tkinter.Label(frame, text = "Point X").grid(row=4, column=0)
 tkinter.Label(frame, text = "Point Y").grid(row=4, column=1)
 tkinter.Entry(frame, textvariable=pointx).grid(row=5, column=0)
 tkinter.Entry(frame, textvariable=pointy).grid(row=5, column=1)
-tkinter.Button(frame, text="create point", command=create_point).grid(row=6, column=0)
+tkinter.Button(frame, text="create point", command=lambda:root.after(1, create_point)).grid(row=6, column=0)
 graphFrom = tkinter.StringVar(frame, "0")
 graphTo = tkinter.StringVar(frame, "50")
 tkinter.Label(frame, text = "Graph from").grid(row=7, column=0)
 tkinter.Label(frame, text = "Graph to").grid(row=7, column=1)
 tkinter.Entry(frame, textvariable=graphFrom).grid(row=8, column=0)
 tkinter.Entry(frame, textvariable=graphTo).grid(row=8, column=1)
-imageFrom = tkinter.StringVar(frame, "0")
-imageTo = tkinter.StringVar(frame, "50")
-tkinter.Label(frame, text = "Image from").grid(row=9, column=0)
-tkinter.Label(frame, text = "Image to").grid(row=9, column=1)
-tkinter.Entry(frame, textvariable=imageFrom).grid(row=10, column=0)
-tkinter.Entry(frame, textvariable=imageTo).grid(row=10, column=1)
-tkinter.Button(frame, text= "import image", command=import_image).grid(row=11, column=0)
-tkinter.Button(frame, text= "delete all points", command=delete_points).grid(row=11, column=1)
+imageFromX = tkinter.StringVar(frame, "0")
+imageToX = tkinter.StringVar(frame, "50")
+imageFromY = tkinter.StringVar(frame, "0")
+imageToY = tkinter.StringVar(frame, "50")
+tkinter.Label(frame, text = "Image from (x,y)").grid(row=9, column=0)
+tkinter.Label(frame, text = "Image to (x,y)").grid(row=9, column=1)
+tkinter.Entry(frame, textvariable=imageFromY).grid(row=10, column=0)
+tkinter.Entry(frame, textvariable=imageToY).grid(row=10, column=1)
+tkinter.Entry(frame, textvariable=imageFromX).grid(row=11, column=0)
+tkinter.Entry(frame, textvariable=imageToX).grid(row=11, column=1)
+tkinter.Button(frame, text= "import image", command=lambda:root.after(1, import_image)).grid(row=12, column=0)
+tkinter.Button(frame, text= "delete all points", command=lambda:root.after(1, delete_points)).grid(row=12, column=1)
+#tkinter.Label(frame, textvariable=status).grid(row=0, column=1)
 create_visualizer()
 root.mainloop()
