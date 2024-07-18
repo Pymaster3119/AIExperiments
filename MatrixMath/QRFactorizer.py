0ac1cd4057206d72eb18846f0bc7685fb24552de
pymaster9 <adi31tya@icloud.com>
2024-07-18 17:23:24 -0400

    Fixing logic errors

--- MatrixMath/QRFactorizer.py
+++ MatrixMath/QRFactorizer.py
@@ -32,9 +32,15 @@ def draw_second_window(reassignVars = True):
                 matrixvars[i].append(tkinter.StringVar(frame))
             tkinter.Entry(frame, textvariable=matrixvars[i][j]).grid(row=j + 1, column=i)
     tkinter.Button(frame, text="Calculate QR Factorization", command= getQR).grid(row=int(matrixHeight.get())+2, column=0)
+    tkinter.Label(frame, text="Q: ").grid(row=int(matrixHeight.get())+3, column=0)
     for j in range(len(Q)):
         for i in range(len(Q[j])):
-            tkinter.Label(frame, textvariable=Q[j][i]).grid(row = i + int(matrixHeight.get())+ 3, column= j)
+            tkinter.Label(frame, textvariable=Q[j][i]).grid(row = i + int(matrixHeight.get())+ 4, column= j)
+
+    tkinter.Label(frame, text="R: ").grid(row=len(Q) + int(matrixHeight.get())+ 5, column=0)
+    for j in range(len(R)):
+        for i in range(len(R[j])):
+            tkinter.Label(frame, textvariable=R[j][i]).grid(row = i + len(Q) + int(matrixHeight.get())+ 6, column= j)
 
 def getQR():
     global Q, R
@@ -44,6 +50,7 @@ def getQR():
         for j in Q:
             currterm = dot(i, j)
             currterm = multiply(currterm, j)
+            print(currterm)
             a = subtract(i, currterm,gets=False)
         if (len(Q) == 0):
             out = []
@@ -52,19 +59,31 @@ def getQR():
             a=out
         length = findlength(a, False)
         Q.append([])
-        for j in i:
+        for j in a:
             Q[len(Q)-1].append(tkinter.StringVar())
-            Q[len(Q)-1][len(Q[len(Q)-1])-1].set(str(float(j.get())/length))
+            Q[len(Q)-1][len(Q[len(Q)-1])-1].set(str(j/length))
+    
+    #Calculating P
+    for i in range(len(matrixvars)):
+        R.append([])
+        for j in range(len(matrixvars[i])):
+            if j <= i:
+                a = matrixvars[i]
+                e = Q[j]
+                result = dot(a,e)
+                R[i].append(tkinter.StringVar())
+                R[i][j].set(str(result))
+            else:
+                R[i].append(tkinter.StringVar())
+                R[i][j].set("0")
     draw_second_window(reassignVars=False)
     
 def findlength(i, get):
     length = 0
-    for j in i:
-        if get:
-            length += float(j.get()) * float(j.get())
-        else:
-            length += j * j
-    length = math.sqrt(length)
+    if get:
+        length = math.sqrt(sum(float(x.get())**2 for x in i))
+    else:
+        length = math.sqrt(sum(x**2 for x in i))
     return length
 
 def dot(i, j):
