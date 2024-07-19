2cd00d49e35f09a10f13d55ae99a7508b8d95866
pymaster9 <adi31tya@icloud.com>
2024-07-18 23:28:32 -0400

    Fixing bugs

--- TopographicalMap/map.py
+++ TopographicalMap/map.py
@@ -1,27 +1,51 @@
-import numpy as np
+import tkinter
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.figure import Figure 
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  
+NavigationToolbar2Tk) 
 
-def plot_3d_function(functionstr):
-    x = np.linspace(-10, 10, 20)
-    y = np.linspace(-10, 10, 20)
-    
+main = tkinter.Tk()
+frame = tkinter.Frame(main)
+frame.pack()
+canvas = tkinter.Canvas(frame)
+canvas.grid(row=0, column=0)
+function = tkinter.StringVar(main)
+function.set("f(x,y): ")
+tkinter.Entry(frame, textvariable=function).grid(row=1, column=0)
+tkinter.Button(frame, command= lambda: plot_function(function.get()), text="Plot function!").grid(row=2, column=0)
+def plot_function(functionstr):
+    global canvas
+    functionstr = functionstr.replace("^", "**").replace("f(x,y):","")
+    x = np.linspace(-100, 100, 200)
+    y = np.linspace(-100, 100, 200)
+    x, y = np.meshgrid(x, y)
     z = eval(functionstr)
 
     fig = plt.figure(figsize=(10, 8))
     axes1 = fig.add_axes([0.1,0.1,0.9,0.9])
-    axes1.set_ylim(-10,10)
-    axes1.set_xlim(-10,10)
+    axes1.set_ylim(-100,100)
+    axes1.set_xlim(-100,100)
 
-    print(z.shape)
-    print(z)
-    for i in x:
-        for j in y:
-            pass
-            
+    maxheight = z.max().item()
+    minheight = z.min().item()
+    contourHeights = np.linspace(minheight,maxheight, 40)
+    print(contourHeights)
+    for i in range(-100, 101):
+        for j in range(-100, 101):
+            x = i
+            y = j
+            height = eval(functionstr)
+            plot = False
+            for k in contourHeights:
+                if abs(height-k) <= 1:
+                    plot = True
+            if plot:
+                color = round((height-minheight)*(255)/(maxheight-minheight))
+                color = '#%02x%02x%02x' % (color, color, color)
+                axes1.plot(i, j, "o", color = color)
+    canvas = FigureCanvasTkAgg(fig,  master = frame)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=0, column=0)
 
-    
-    plt.show()
-
-if __name__ == "__main__":
-    func_str = input("Enter a 3-D function of x and y (e.g., 'np.sin(np.sqrt(x**2 + y**2))', 'x**2 - y**2', 'np.exp(-x**2 - y**2)'): ")
-    plot_3d_function(func_str)
+main.mainloop()
