8b73aceb91c65d5a3063289b7ce3a4088c325a5c
pymaster9 <adi31tya@icloud.com>
2024-07-19 22:20:57 -0400

    Fixing math issues with my FFNN

--- FFNNLineDetector/LineDetectorFFNN.py
+++ FFNNLineDetector/LineDetectorFFNN.py
@@ -5,7 +5,7 @@ slope = 5
 points = []
 batch = 0
 bestlayer = 0
-accuracytobeat = 0
+accuracytobeat = 10000000000000000000
 
 class point:
     def __init__ (self, x, y, classification, truth):
@@ -23,20 +23,20 @@ def plot():
     averageaccuracy = 0
     for x in points:
 
-        if (x.classification[0] > 0.5 and x.truth == True):
+        if (x.classification[0,0] > 0.5 and x.truth == True):
             truepositives += 1
-        if (x.classification[0] < 0.5 and x.truth == True):
+        if (x.classification[0,0] < 0.5 and x.truth == True):
             falsenegatives += 1
-        if (x.classification[0] > 0.5 and x.truth == False):
+        if (x.classification[0,0] > 0.5 and x.truth == False):
             falsepositives += 1
-        if (x.classification[0] < 0.5 and x.truth == False):
+        if (x.classification[0,0] < 0.5 and x.truth == False):
             truenegatives += 1
-        averageaccuracy += 0.5 * (x.classification - x.truth) ** 2
+        averageaccuracy += 0.5 * (x.classification[0,0] - x.truth) ** 2
     accuracy =(truenegatives + truepositives)/len(points)
     averageaccuracy /= len(points)
-    if (accuracy > accuracytobeat):
+    if (averageaccuracy < accuracytobeat):
         bestlayer = batch
-        accuracytobeat = accuracy
+        accuracytobeat = averageaccuracy
 
     with (open("FFNNLineDetector/ModelStats/" + str(batch) +'.txt', "w") as txt):
         txt.write("True Negatives: " + str(truenegatives) + "\n")
@@ -44,21 +44,18 @@ def plot():
         txt.write("True Positives: " + str(truepositives) + "\n")
         txt.write("False Positives: " + str(falsepositives) + "\n")
         txt.write("Accuracy: " + str(accuracy) + "\n")
-        txt.write("Average Accuracy: " + str(averageaccuracy[0]))
+        txt.write("Average Accuracy: " + str(averageaccuracy))
 
 
 def ReLu(val):
-    output = numpy.zeros(val.shape[0])
-    for x in range(val.shape[0]):
-        output[x] = max(min(val[x], 1), 0)
-    return output
+    return numpy.maximum(0, numpy.minimum(1, val))
 
 def relu_derivative(x):
     return numpy.where((x > 0) & (x < 1), 1, 0)
 #Set up layers
 inputLayerNeuronCount = 4
-z1LayerNeuronCount = 10
-z2LayerNeuronCount = 10
+z1LayerNeuronCount = 5
+z2LayerNeuronCount = 5
 outputLayerNeuronCount = 1
 
 #Initialize weights
@@ -67,22 +64,22 @@ z1ToZ2Weights = numpy.random.rand(z2LayerNeuronCount, z1LayerNeuronCount)
 z2ToOutputWeights = numpy.random.rand(outputLayerNeuronCount, z2LayerNeuronCount)
 
 #Initialize biases
-inputLayerToZ1Biases = numpy.random.rand(z1LayerNeuronCount)
-z1ToZ2Biases = numpy.random.rand(z2LayerNeuronCount)
-z2ToOutputBiases = numpy.random.rand(outputLayerNeuronCount)
+inputLayerToZ1Biases = numpy.random.rand(z1LayerNeuronCount,1)
+z1ToZ2Biases = numpy.random.rand(z2LayerNeuronCount,1)
+z2ToOutputBiases = numpy.random.rand(outputLayerNeuronCount,1)
 
 #Training parameters
-learning_rate = 0.01
+learning_rate = 0.001
 #Mainloop
-for batch in range(100):
+for batch in range(1000):
     points = []
-    for d in range(100):
+    for d in range(1000):
         #Set parameters for current iteration & formulate input layer
         y_int = random.random() * 400 - 200
         slope = random.random() * 20 - 10
         inputx=random.random() * 100
         inputy=random.random() * 100
-        inputlayer = numpy.array([y_int, slope, inputx, inputy])
+        inputlayer = numpy.array([[y_int], [slope], [inputx], [inputy]])
 
         
         #Run the model - input -> z1
@@ -102,29 +99,24 @@ for batch in range(100):
         #Evaluate and save results
         truth = 1 if inputy > (slope * inputx) + y_int else 0
         points.append(point(inputx, inputy, output, truth))
-        error = 0.5 * (output[0] - truth) ** 2
+        error = 0.5 * (output[0, 0] - truth) ** 2
 
         #Storing updated weights as temp vars to make sure that there is no conflict
         #Backwards pass - output -> z2
-        lossgradient = (output[0] - truth)
-        z2ToOutputWeightsTemp = numpy.subtract(z2ToOutputWeights, learning_rate * lossgradient * ReLu(preactivationZ2))
-        z2ToOutputBiasesTemp = numpy.subtract(z2ToOutputBiases, learning_rate * lossgradient)
+        lossgradient = (output[0, 0] - truth)
+        z2ToOutputWeightsTemp = z2ToOutputWeights - learning_rate * lossgradient * x2.T
+        z2ToOutputBiasesTemp = z2ToOutputBiases - learning_rate * lossgradient
 
         #Backwards pass - z2 -> z1
-        gradientz2 = numpy.dot(lossgradient, z2ToOutputWeights) * relu_derivative(preactivationZ2)
-        z2delta = numpy.dot(lossgradient, z2ToOutputWeights.T) * relu_derivative(preactivationZ2)
-        z1ToZ2WeightsChange = numpy.dot(z2delta,x1)
-        z1ToZ2BiasChange = numpy.dot(lossgradient, z2ToOutputWeights.T) * relu_derivative(preactivationZ2)
-        z1ToZ2WeightsTemp = numpy.subtract(z1ToZ2Weights, learning_rate * z1ToZ2WeightsChange)
-        z1ToZ2BiasesTemp = numpy.subtract(z1ToZ2Biases, learning_rate * z1ToZ2BiasChange)
+        gradientz2 = lossgradient * z2ToOutputWeights.T * relu_derivative(preactivationZ2)  # <---- Corrected gradient calculation
+        z1ToZ2WeightsTemp = z1ToZ2Weights - learning_rate * numpy.dot(gradientz2, x1.T)  # <---- Corrected gradient calculation
+        z1ToZ2BiasesTemp = z1ToZ2Biases - learning_rate * gradientz2
+        
 
         #Backwards pass - input -> z1
-        
-        inputLayerToZ1WeightsChange = numpy.dot(z1ToZ2Weights.T, numpy.dot(lossgradient, z2ToOutputWeights.T) * relu_derivative(preactivationZ2)) * relu_derivative(preactivationZ1)
-        inputLayerToZ1WeightsDelta = numpy.dot(inputLayerToZ1WeightsChange, inputlayer.T)
-        inputLayerToZ1BiasesDelta = numpy.sum(inputlayer, axis=1, keepdims=True)
-        inputLayerToZ1WeightsTemp = numpy.subtract(inputLayerToZ1Weights, learning_rate * inputLayerToZ1WeightsDelta)
-        inputLayerToZ1BiasesTemp = numpy.subtract(inputLayerToZ1Biases, learning_rate * inputLayerToZ1BiasesDelta)
+        gradientz1 = numpy.dot(z1ToZ2Weights.T, gradientz2) * relu_derivative(preactivationZ1)
+        inputLayerToZ1WeightsTemp = inputLayerToZ1Weights - learning_rate * numpy.dot(gradientz1, inputlayer.T)
+        inputLayerToZ1BiasesTemp = inputLayerToZ1Biases - learning_rate * gradientz1
 
         #Update weights - replace the weights + biases with the temporary ones calculated
         inputLayerToZ1Weights = inputLayerToZ1WeightsTemp
